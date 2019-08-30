42dacf9a0d3c9ace976997737102f92efa1c2e2f
francisudeji <francis.udejiofficial@gmail.com>
2019-08-30 09:59:35 +0100

    Finished design

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      width: {
+        '48%': '48%',
+        '23%': '23%',
+        '31%': '31%'
+      },
+      colors: {
+        'tw-lightest-blue': '#37517e',
+        'tw-light-blue': '#00b9ff',
+        'tw-dark-blue': '#2e4369',
+        'tw-green': '#2ed06e'
+      },
+      backgroundColors: {
+        'tw-lightest-blue': '#37517e',
+        'tw-light-blue': '#00b9ff',
+        'tw-dark-blue': '#2e4369',
+        'tw-green': '#2ed06e'
+      }
+    },
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    }
+  }
+}
